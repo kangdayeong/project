843cc260227e11b85063a4382b5d6d9569007e87
kangdayeong <101917092+kangdayeong@users.noreply.github.com>
2022-12-17 17:21:53 +0900

    Add files via upload

---
+++ EventDetails.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace Calendar.NET
+{
+    partial class EventDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtDate = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEventName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.DetailTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.InputPeopleName = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fontDialog1
+            // 
+            this.fontDialog1.ScriptsOnly = true;
+            // 
+            // btnOk
+            // 
+            this.btnOk.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnOk.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnOk.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOk.Font = new System.Drawing.Font("굴림", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnOk.ForeColor = System.Drawing.Color.White;
+            this.btnOk.Location = new System.Drawing.Point(50, 261);
+            this.btnOk.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(82, 26);
+            this.btnOk.TabIndex = 7;
+            this.btnOk.Text = "&OK";
+            this.btnOk.UseVisualStyleBackColor = false;
+            this.btnOk.Click += new System.EventHandler(this.BtnOkClick);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.LightGray;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Font = new System.Drawing.Font("굴림", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnCancel.Location = new System.Drawing.Point(163, 261);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(81, 26);
+            this.btnCancel.TabIndex = 8;
+            this.btnCancel.Text = "&Cancel";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.BtnCancelClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 22);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 12);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "현장 날짜";
+            // 
+            // dtDate
+            // 
+            this.dtDate.CustomFormat = "yyyy/M/d";
+            this.dtDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtDate.Location = new System.Drawing.Point(125, 16);
+            this.dtDate.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.dtDate.Name = "dtDate";
+            this.dtDate.Size = new System.Drawing.Size(232, 21);
+            this.dtDate.TabIndex = 10;
+            this.dtDate.Value = new System.DateTime(2022, 12, 8, 0, 0, 0, 0);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 60);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 12);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "현장 이름";
+            // 
+            // txtEventName
+            // 
+            this.txtEventName.Location = new System.Drawing.Point(125, 57);
+            this.txtEventName.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.txtEventName.Multiline = true;
+            this.txtEventName.Name = "txtEventName";
+            this.txtEventName.Size = new System.Drawing.Size(232, 21);
+            this.txtEventName.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "세부사항";
+            // 
+            // DetailTextBox
+            // 
+            this.DetailTextBox.Location = new System.Drawing.Point(51, 140);
+            this.DetailTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.DetailTextBox.Multiline = true;
+            this.DetailTextBox.Name = "DetailTextBox";
+            this.DetailTextBox.Size = new System.Drawing.Size(306, 118);
+            this.DetailTextBox.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(48, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 12);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "투입 인원";
+            // 
+            // InputPeopleName
+            // 
+            this.InputPeopleName.Location = new System.Drawing.Point(126, 93);
+            this.InputPeopleName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.InputPeopleName.Multiline = true;
+            this.InputPeopleName.Name = "InputPeopleName";
+            this.InputPeopleName.ReadOnly = true;
+            this.InputPeopleName.Size = new System.Drawing.Size(231, 41);
+            this.InputPeopleName.TabIndex = 16;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.IndianRed;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("굴림", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(276, 261);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 26);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Delete";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Calendar.NET.Properties.Resources.free_icon_edit_860814;
+            this.pictureBox1.Location = new System.Drawing.Point(7, 14);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(41, 35);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            // 
+            // EventDetails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(406, 298);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.InputPeopleName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.DetailTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtEventName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtDate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOk);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "EventDetails";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "EventDetails";
+            this.Load += new System.EventHandler(this.EventDetailsLoad);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtDate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEventName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox DetailTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox InputPeopleName;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ EventDetails.cs
@@ -0,0 +1,175 @@
+﻿using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI.Relational;
+using System;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Tab;
+
+namespace Calendar.NET
+{
+    public partial class EventDetails : Form
+    {
+        string _server = "localhost";
+        int _port = 3306;
+        string _database = "calendardb";
+        string _id = "root";
+        string _pw = "dy12121004?";
+        string _connectionAddress = "";
+        [CustomRecurringFunction("RehabDates", "Calculates which days I should be getting Rehab")]
+
+        private IEvent _event;
+        private IEvent _newEvent;
+
+        public IEvent Event
+        {
+            get { return _event; }
+            set
+            {
+                _event = value;
+                FillValues();
+            }
+        }
+
+        public IEvent NewEvent
+        {
+            get { return _newEvent; }
+        }
+
+        public EventDetails()
+        {
+            InitializeComponent();
+            _connectionAddress = string.Format("Server={0};Port={1};Database={2};Uid={3};Pwd={4}", _server, _port, _database, _id, _pw);
+        }
+
+        private void EventDetailsLoad(object sender, EventArgs e)
+        {
+            String fisrtDay = _event.Date.ToString("yyyy/M/d");
+            try
+            {
+                using (MySqlConnection mysql = new MySqlConnection(_connectionAddress))
+                {
+                    mysql.Open();
+                    String People = "";
+                    string selectCalendarDetail = ($"select * from 현장 where CalendarDay='{fisrtDay}';");
+                    string selectPeopleName = ($"select * from 인원 where PeopleDay='{fisrtDay}';");
+
+                    MySqlCommand command1 = new MySqlCommand(selectCalendarDetail, mysql);
+                    MySqlDataReader table1 = command1.ExecuteReader();
+                    while (table1.Read())
+                    {
+                        DetailTextBox.Text = table1["CalendarDetail"].ToString();
+                    }
+                    table1.Close();
+                    MySqlCommand command2 = new MySqlCommand(selectPeopleName, mysql);
+                    MySqlDataReader table2 = command2.ExecuteReader();
+                    while (table2.Read())
+                    {
+                        People += table2["PeopleName"].ToString()+" ";
+                    }
+                    table2.Close();
+                    InputPeopleName.Text = People;
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void FillValues()
+        {
+            txtEventName.Text = _event.EventText;
+            dtDate.Value = _event.Date;
+            dtDate.CustomFormat = _event.IgnoreTimeComponent ? "yyyy/M/d" : "yyyy/M/d";//시간 찾기
+
+            Text = char.ToUpper(_event.EventText[0]) + _event.EventText.Substring(1) + " Details";
+
+            _newEvent = _event.Clone();
+        }
+
+        private void BtnOkClick(object sender, EventArgs e)
+        {
+            _newEvent.EventText = txtEventName.Text;
+            _newEvent.Date = dtDate.Value;
+
+            String fisrtDay = _event.Date.ToString("yyyy/M/d");
+            String text = txtEventName.Text;
+            String day = dtDate.Value.ToString("yyyy/M/d");
+            String detail = DetailTextBox.Text;
+
+            try
+            {
+                using (MySqlConnection mysql = new MySqlConnection(_connectionAddress))
+                {
+                    mysql.Open();
+                    string diableSafe = "SET SQL_SAFE_UPDATES = 0";
+                    string selectQuery = ($"update 현장 set CalendarField='{text}', CalendarDay='{day}', CalendarDetail='{detail}' where CalendarDay='{fisrtDay}';");
+                    string ableSafe = "SET SQL_SAFE_UPDATES = 1";
+
+                    MySqlCommand command1 = new MySqlCommand(diableSafe, mysql);
+                    MySqlDataReader table1 = command1.ExecuteReader();
+                    table1.Close();
+                    MySqlCommand command2 = new MySqlCommand(selectQuery, mysql);
+                    MySqlDataReader table2 = command2.ExecuteReader();
+                    table2.Close();
+                    MySqlCommand command3 = new MySqlCommand(ableSafe, mysql);
+                    MySqlDataReader table3 = command3.ExecuteReader();
+                    table3.Close();
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+            Close();
+            DialogResult = DialogResult.OK;
+        }
+
+        private void BtnCancelClick(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String day = dtDate.Value.ToString("yyyy/M/d");
+            _newEvent.Date = Convert.ToDateTime("2020-01-01");
+            DialogResult = DialogResult.OK;
+
+            try
+            {
+                using (MySqlConnection mysql = new MySqlConnection(_connectionAddress))
+                {
+                    mysql.Open();
+                    string diableSafe = "SET SQL_SAFE_UPDATES = 0";
+                    string deletePeople = ($"update 인원 set PeopleDay= {0000-00-00} where PeopleDay='{day}';");
+                    string selectQuery = ($"delete from 현장 where CalendarDay='{day}'");
+                    string ableSafe = "SET SQL_SAFE_UPDATES = 1";
+
+                    MySqlCommand command1 = new MySqlCommand(diableSafe, mysql);
+                    MySqlDataReader table1 = command1.ExecuteReader();
+                    table1.Close();
+                    MySqlCommand command2 = new MySqlCommand(selectQuery, mysql);
+                    MySqlDataReader table2 = command2.ExecuteReader();
+                    table2.Close();
+                    MySqlCommand command3 = new MySqlCommand(deletePeople, mysql);
+                    MySqlDataReader table3 = command3.ExecuteReader();
+                    table3.Close();
+                    MySqlCommand command4 = new MySqlCommand(ableSafe, mysql);
+                    MySqlDataReader table4 = command4.ExecuteReader();
+                    table4.Close();
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+            Close();
+        }
+    }
+}
